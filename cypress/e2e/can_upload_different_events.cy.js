ce865250c58342a9ad94d68cbff0bd63390bd5b2
Dylan Broadbridge <9392694+Dylanb-dev@users.noreply.github.com>
2023-02-06 13:51:53 +0800

    Revert "rename test events, add update script"

--- cypress/e2e/can_upload_different_events.cy.js
+++ cypress/e2e/can_upload_different_events.cy.js
@@ -6,22 +6,26 @@ describe('Test other events can be uploaded', () => {
       cy.login()
       cy.visit('/')
   
-      cy.fixture('Antares_ignore_observation_event.txt').then((event1) => {
+      //upload lvc "real" event that we want to trigger on
+      cy.fixture('Antares_1438351269.txt').then((event1) => {
         cy.get('[data-testid="nav-testing"]').click({ force: true })
         cy.get('[class="form-control"]').invoke('val', (event1))
         cy.get("[type='submit']").click()
       })
-      cy.fixture('Fermi_ignore_observation_event.txt').then((event1) => {
+      //upload lvc "real" event that we want to trigger on
+      cy.fixture('Fermi00.txt').then((event1) => {
         cy.get('[data-testid="nav-testing"]').click({ force: true })
         cy.get('[class="form-control"]').invoke('val', event1)
         cy.get("[type='submit']").click()
       })
-      cy.fixture('HESS_promising_observation_event.txt').then((event1) => {
+          //upload lvc "real" event that we want to trigger on
+      cy.fixture('HESS_test_event_real_promising.txt').then((event1) => {
         cy.get('[data-testid="nav-testing"]').click({ force: true })
         cy.get('[class="form-control"]').invoke('val', event1)
         cy.get("[type='submit']").click()
       })
-      cy.fixture('SWIFT_promising_observation_event.txt').then((event1) => {
+      //upload lvc "real" event that we want to trigger on
+      cy.fixture('SWIFT_2018_03_25.txt').then((event1) => {
         cy.get('[data-testid="nav-testing"]').click({ force: true })
         cy.get('[class="form-control"]').invoke('val', event1)
         cy.get("[type='submit']").click()
