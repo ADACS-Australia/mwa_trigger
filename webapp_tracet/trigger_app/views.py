8db014e0adb63088c718d204773f1e4e1359404b
Dylan <292206H@curtin.edu.au>
2023-07-17 16:30:41 +0800

    update recent decisions lvc view

--- webapp_tracet/trigger_app/views.py
+++ webapp_tracet/trigger_app/views.py
@@ -295,16 +295,21 @@ def home_page(request):
     prop_settings = models.ProposalSettings.objects.all()
 
     # Filter out ignored event groups and telescope=swift and show only the 5 most recent
-    recent_event_groups = models.EventGroup.objects.filter(
+    recent_event_groups_swift = models.EventGroup.objects.filter(
         ignored=False, source_type="GRB")
-    recent_event_group_info, _ = grab_decisions_for_event_groups(
-        recent_event_groups)
+    recent_event_group_info_swift, _ = grab_decisions_for_event_groups(
+        recent_event_groups_swift)
 
     recent_event_group_info_swift = filter(
-        lambda x: x[1] == "SWIFT", recent_event_group_info)
+        lambda x: x[1] == "SWIFT", recent_event_group_info_swift)
+
+    recent_event_groups_lvc = models.EventGroup.objects.filter(
+        ignored=False, source_type="GW")
+    recent_event_group_info_lvc, _ = grab_decisions_for_event_groups(
+        recent_event_groups_lvc)
 
     recent_event_group_info_lvc = filter(
-        lambda x: x[1] == "LVC", recent_event_group_info)
+        lambda x: x[1] == "LVC", recent_event_group_info_lvc)
 
     context = {
         'twistd_comet_status': comet_status,
