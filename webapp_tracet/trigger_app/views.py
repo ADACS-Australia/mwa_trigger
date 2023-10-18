196c55d030c2223c94e85361c54ab5507237cd8d
Dylan <292206H@curtin.edu.au>
2023-10-18 17:33:02 +0800

    update swift previews

--- webapp_tracet/trigger_app/views.py
+++ webapp_tracet/trigger_app/views.py
@@ -393,9 +393,15 @@ def home_page(request):
 
     prop_settings = models.ProposalSettings.objects.all()
 
+    req = QueryDict("ignored=False&source_type=GRB&telescope=SWIFT")
+
+    f = EventGroupFilter(req, queryset=models.EventGroup.objects.distinct().filter(voevent__role="observation"))
+    recent_event_groups_swift = f.qs[:20]
+
+
     # Filter out ignored event groups and telescope=swift and show only the 5 most recent
-    recent_event_groups_swift = models.EventGroup.objects.filter(
-        ignored=False, source_type="GRB")[:20]
+    # recent_event_groups_swift = models.EventGroup.objects.filter(
+    #     ignored=False, source_type="GRB")[:20]
     recent_event_group_info_swift, _ = grab_decisions_for_event_groups(
         recent_event_groups_swift)
 
