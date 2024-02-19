1a2e9557c10140d543b9074c525ff9c91219228d
PaulHancock <paul.hancock@curtin.edu.au>
2024-02-19 08:18:37 +0000

    add back deleted file

---
+++ webapp_tracet/upload_xml.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import requests
+
+from tracet.parse_xml import parsed_VOEvent
+import logging
+logger = logging.getLogger(__name__)
+
+
+def write_and_upload(xml_string):
+
+    # Upload
+    session = requests.session()
+    session.auth = (os.environ['UPLOAD_USER'], os.environ['UPLOAD_PASSWORD'])
+    SYSTEM_ENV = os.environ.get('SYSTEM_ENV', None)
+    if SYSTEM_ENV == 'PRODUCTION' or SYSTEM_ENV == 'STAGING':
+        url = 'https://tracet.duckdns.org/event_create/'
+    else:
+        url = 'http://127.0.0.1:8000/event_create/'
+
+    data = {
+        'xml_packet': xml_string
+    }
+    session.post(url, data=data)
+
+if __name__ == '__main__':
+    xml_string = sys.stdin.read()
+    write_and_upload(xml_string)
