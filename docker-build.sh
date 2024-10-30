f544a984697483e959230385daa844ca5ef477b2
oldyoungcoders <bsscorpio47@gmail.com>
2024-10-30 13:46:26 +0800

    docker_build.sh script added

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+mkdir -p prop_api/logs
+
+docker-compose build 
+
+sleep 2s
+docker-compose up -d 
+
+sleep 15s
+
+docker cp ../demo_trigger_db.sql db-container:/
+
+sleep 5s
+docker exec -it db-container bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql -U trigger_admin -d trigger_db -f /demo_trigger_db.sql"
+
+sleep 5s
 docker-compose down
 
-# Perform cleanup
-docker volume rm $(docker volume ls -q)
-docker volume prune -f
-docker container prune -f
-docker system prune -a -f
+sleep 5s
 
-# Build and run Docker containers
-docker-compose build
-docker-compose up
+docker-compose up -d
+
+echo "Deployment completed successfully."
